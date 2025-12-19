6fb0e9910c58778594d3aed651e8aaf004357d80
MohammedRiyazdeen <ariyazdeen128@gmail.com>
2025-12-19 23:11:21 +0530

    Created Custom User Model

--- identity/models.py
+++ identity/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    STUDENT = 'student'
+    TEACHER = 'teacher'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = [
+        (STUDENT, 'Student'),
+        (TEACHER, 'Teacher'),
+        (ADMIN, 'Admin'),
+    ]
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=STUDENT
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
